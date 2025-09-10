4f0ff151740028be6d75ec5fbd151bb8fe6856bd
Drake Pollard <17drakester@gmail.com>
2025-09-10 12:37:11 -0400

    v0.2.0.3

--- main.cpp
+++ main.cpp
@@ -2,15 +2,20 @@
 #include "src/TruthTables.h"
 
 int main(){
-
+    Table truth;
+    truth.readfunc();
+    truth.printTable("Jeff");
     //Used to input the amount of variables.
+    /*
     std::cout<<"How many variables do you need?: ";
     int input;
     std::cin>>input;
     Table truth(input);
     Table truthtable(input);
+    */
    // truth.writeTable();
     //truth.printTable();
+    /*
     std::cout<<"\nTable 1: AND ::\n";
     truth.op_and();
     truth.printTable("yo");
@@ -18,6 +23,6 @@ int main(){
     std::cout<<"\nTable 2: OR :: \n";
     truthtable.op_or();
     truthtable.printTable("yo!");
-
+*/
     return 0;
 }
